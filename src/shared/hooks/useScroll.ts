897eec6a3eddac6ac7d911b024746096ed0be995
Hishchak Tetiana <tanaiosered@ukr.net>
2025-03-09 15:31:26 +0200

    change scroll function

--- src/shared/hooks/useScroll.ts
+++ src/shared/hooks/useScroll.ts
@@ -1,21 +1,23 @@
 import { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
 import { useThemeStore } from '~/shared/stores/theme.store';
 
-
 export const useScroll = () => {
-	const [isScrolled, setIsScrolled] = useState(false);
-	const toggleTheme = useThemeStore((state) => state.toggleTheme);
+  const [isScrolled, setIsScrolled] = useState(false);
+  const toggleTheme = useThemeStore((state) => state.toggleTheme);
+  const { pathname } = useLocation();
 
-	useEffect(() => {
-		const handleScroll = () => {
-			const scrolled = window.scrollY > 34;
-			setIsScrolled(scrolled);
-			toggleTheme(scrolled);
-		};
+  useEffect(() => {
+    if (pathname !== '/') return; 
+    const handleScroll = () => {
+      const scrolled = window.scrollY > 34;
+      setIsScrolled(scrolled);
+      toggleTheme(scrolled);
+    };
 
-		window.addEventListener('scroll', handleScroll);
-		return () => window.removeEventListener('scroll', handleScroll);
-	}, [toggleTheme]);
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, [toggleTheme, pathname]);
 
-	return isScrolled;
+  return isScrolled;
 };
