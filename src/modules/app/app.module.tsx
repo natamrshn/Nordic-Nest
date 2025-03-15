71d3f8e19aa8e214222c8b65a725bcb26bd9d905
Nataliia Miroshnyk <natamrshn@gmail.com>
2025-03-15 10:21:30 +0000

    fix header

--- src/modules/app/app.module.tsx
+++ src/modules/app/app.module.tsx
@@ -8,48 +8,65 @@ import Navigation from '~shared/components/header/components/navigation/navigati
 import Space from '~shared/components/space/space';
 import Furniture from '~shared/components/furniture/furniture';
 import ModalImg from '~shared/components/modalImg/modalImg';
+import { useThemeStore } from '~shared/stores/theme.store';
+import { useLocation } from 'react-router-dom';
 
 const App = (): React.ReactNode => {
+	const { pathname } = useLocation();
+	console.log(pathname);
+	const setIsLight = useThemeStore((state) => state.setIsLight);
+
+	React.useEffect(() => {
+		setIsLight(pathname !== '/');
+	}, [pathname]);
+
 	const [isModalOpen, setIsModalOpen] = React.useState(false);
-  const modalRef = React.useRef(null);
+	const modalRef = React.useRef(null);
 
-  const handleOutsideClick = (event) => {
-    if (modalRef.current && !modalRef.current.contains(event.target)) {
-        setIsModalOpen(false);
-    }
-  };
+	const handleOutsideClick = (event) => {
+		if (modalRef.current && !modalRef.current.contains(event.target)) {
+			setIsModalOpen(false);
+		}
+	};
 
-React.useEffect(() => {
-  document.addEventListener("mousedown", handleOutsideClick);
-  return () => {
-      document.removeEventListener("mousedown", handleOutsideClick);
-  };
-}, []);
+	React.useEffect(() => {
+		document.addEventListener('mousedown', handleOutsideClick);
+		return () => {
+			document.removeEventListener('mousedown', handleOutsideClick);
+		};
+	}, []);
 
-  const handleShopClick = () => {
-    setIsModalOpen(prevState => !prevState);
-  };
-    return (
-      <>
-        <PromoBanner />
-        <Header onShopClick={handleShopClick}/>
-        <Navigation  isModalOpen={isModalOpen} onShopClick={handleShopClick} /> 
-        <MainRouter />
-        <Footer />
+	const handleShopClick = () => {
+		setIsModalOpen((prevState) => !prevState);
+	};
+	return (
+		<>
+			<PromoBanner />
+			<Header onShopClick={handleShopClick} />
+			<Navigation
+				isModalOpen={isModalOpen}
+				onShopClick={handleShopClick}
+			/>
+			<MainRouter />
+			<Footer />
 
-
-        <Modal   isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
-          <div  ref={modalRef} style={{
-            display: 'flex', 
-            width: '100%', justifyContent: 'space-between', padding: '70px 80px',}}
-          >
-            <Space />
-            {/* <Furniture /> */}
-            <ModalImg />
-          </div>
-          </Modal>
-      </>
-    );
+			<Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
+				<div
+					ref={modalRef}
+					style={{
+						display: 'flex',
+						width: '100%',
+						justifyContent: 'space-between',
+						padding: '70px 80px',
+					}}
+				>
+					<Space />
+					{/* <Furniture /> */}
+					<ModalImg />
+				</div>
+			</Modal>
+		</>
+	);
 };
 
 export default App;
